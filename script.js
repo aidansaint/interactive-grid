6ee2d67899217036347309773d16c2ad684a4595
Aidan Stewart-Thomson <165275972+aidansaint@users.noreply.github.com>
2025-01-04 11:49:52 +1100

    implement tile hover effect

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const colours = [
+  "rgb(255, 60, 50)", // red
+  "rgb(255, 150, 0)", // orange
+  "rgb(255, 205, 0)", // yellow
+  "rgb(50, 200, 90)", // green
+  "rgb(0, 200, 190)", // mint
+  "rgb(50, 175, 200)", // teal
+  "rgb(50, 175, 230)", // cyan
+  "rgb(0, 120, 255)", // blue
+  "rgb(90, 85, 215)", // indigo
+  "rgb(175, 80, 220)", // purple
+  "rgb(255, 45, 85)", // pink
+  "rgb(165, 130, 95)", // brown
+];
+
+function getRandomColour() {
+  const randomIndex = Math.floor(Math.random() * colours.length);
+
+  return colours[randomIndex];
+}
+
 const container = document.querySelector(".container");
 
 function createTiles(numberOfTiles) {
@@ -11,5 +32,17 @@ function createTiles(numberOfTiles) {
 
   container.appendChild(fragment);
 }
+
+container.addEventListener("mouseover", (e) => {
+  if (e.target.classList.contains("tile")) {
+    e.target.style.backgroundColor = getRandomColour();
+  }
+});
+
+container.addEventListener("mouseout", (e) => {
+  if (e.target.classList.contains("tile")) {
+    e.target.style.backgroundColor = "";
+  }
+});
 
 createTiles(4096);
